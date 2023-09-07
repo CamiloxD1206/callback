c0ecef1566d0d1bcedf44bef0464c0c1b3583de6
Camilo Tacue Salazar <cetacue@misena.edu.co>
2023-09-06 23:33:41 -0500

    callbacks 2.0

--- js/app.js
+++ js/app.js
@@ -21,21 +21,38 @@
 
 // setTimeout(() => primerAccion(() => segundaAccion('esta aprendiendo callbacks'), 'Camilo Estevan '), 5000)
 //----------------------------------------------------------------------------------------------------------
-function nombres(z, g) {
-    console.log(g)
-    setTimeout(z, 2000)
+// function nombres(z, g) {
+//     console.log(g)
+//     setTimeout(z, 2000)
+// }
+
+// function apellidos(s, r) {
+//     console.log(r)
+//     setTimeout(s, 2000)
+
+// }
+
+// function informacion(v) {
+//     console.log(v)
+
+// }
+// setTimeout(() => nombres(() => apellidos(() => informacion('tengo 20 años,estudio desarrollo de software y mido 1.62cm'), 'Tacue Salazar'), 'Camilo Estevan'), 2000)
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//concatenadora con callback
+
+
+function peticion(z, s) {
+    console.log('Realizando tu operación, espera un momento');
+    setTimeout(() => mensaje(z, s), 1000);
 }
 
-function apellidos(s, r) {
-    console.log(r)
-    setTimeout(s, 2000)
-
-}
-
-function informacion(v) {
-    console.log(v)
-
+function mensaje(z, s) {
+    console.log(`La suma de ${z} + ${s} es igual a: ${z + s}`);
 }
 
 
-setTimeout(() => nombres(() => apellidos(() => informacion('tengo 20 años,estudio desarrollo de software y mido 1.62cm'), 'Tacue Salazar'), 'Camilo Estevan'), 2000)
+setTimeout(() => {
+    const numero1 = parseFloat(prompt('Dame el primer número'));
+    const numero2 = parseFloat(prompt('Dame el segundo número'));
+    peticion(numero1, numero2);
+}, 3000);
